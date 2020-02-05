ee2fb90bf54fea40e12bc1da5aef62363b8d709b
vagrant <vagrant@vagrant.vm>
2020-02-05 00:04:12 +0000

    fixing logic

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,12 +3,18 @@
 
 
 def canUnlockAll(boxes):
+    locked = {box: True for box in range(1, len(boxes))}
+    locked.update({0: False})
     keys = boxes[0]
-
-    for i in range(len(boxes) - 1):
-        if i + 1 in keys:
-            keys = keys + boxes[i + 1]
-        else:
-            return False
-
+    unlock(boxes, locked, keys)
+    
+    if True in locked.values():
+        return False
     return True
+
+
+def unlock(boxes, locked, keys):
+    for key in keys:
+        if locked[key]:
+            locked[key] = False
+            unlock(boxes, locked, boxes[key])
