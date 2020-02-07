60d92ee9903fe364a42ca3977dacff0af839b3d6
vagrant <vagrant@vagrant.vm>
2020-02-07 20:49:17 +0000

    Add node to linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - adds a new ordered node
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new;
+    listint_t *current;
+    current = *head;
+    if (*head == NULL)
+        return (NULL);
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = number;
+
+    while (current->next != NULL && current->next->n < number)
+        current = current->next;
+
+    if (current->next == NULL)
+        new->next = NULL;
+    else
+        new->next = current->next;
+
+    current->next = new;
+
+    return (new);
+}
