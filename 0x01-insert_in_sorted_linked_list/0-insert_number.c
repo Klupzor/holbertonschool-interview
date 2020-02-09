5a5fd0c790aec0d3e5ad2c396e80d33314b2fe54
vagrant <vagrant@vagrant.vm>
2020-02-09 23:44:27 +0000

    Fixing case error

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -24,6 +24,12 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	new->n = number;
+	if (number < current->n)
+	{
+		new->next = current;
+		*head = new;
+		return (new);
+	}
 	while (current->next != NULL && current->next->n < number)
 		current = current->next;
 
