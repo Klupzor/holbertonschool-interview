f80b510e5dc46b16b2574d0440cdbac08944009c
vagrant <vagrant@vagrant.vm>
2020-02-10 01:56:47 +0000

    improving performance

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -30,11 +30,7 @@ listint_t *insert_node(listint_t **head, int number)
 	while (current->next != NULL && current->next->n < number)
 		current = current->next;
 
-	if (current->next == NULL)
-		new->next = NULL;
-	else
-		new->next = current->next;
-
+	new->next = current->next;
 	current->next = new;
 
 	return (new);
--- 0x01-insert_in_sorted_linked_list/README.md
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -1,1 +1,3 @@
 # 0x01. Insert in sorted linked list
+
+compile with: `gcc -Wall -Werror -Wextra -pedantic 0-main.c linked_lists.c 0-insert_number.c -o insert`
