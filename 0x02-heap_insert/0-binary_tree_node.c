4cb4c105eb4296f934cc288c31c48dac15390384
vagrant <vagrant@vagrant.vm>
2020-02-18 20:09:50 +0000

    fixing styles

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,16 +1,26 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
-    binary_tree_t *new;
+/**
+ * binary_tree_node - Create new node
+ *
+ * @parent: Pointer to the father node
+ * @value: Node Value
+ *
+ * Return: new node
+ */
 
-    new = malloc(sizeof(binary_tree_t));
-    if (new == NULL)
-        return (NULL);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
 
-    new->parent = parent;
-    new->n = value;
-    new->left= NULL;
-    new->right = NULL;
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
 
-    return (new);
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
 }
