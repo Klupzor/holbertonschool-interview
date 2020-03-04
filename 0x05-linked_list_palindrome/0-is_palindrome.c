950c7c35d6c01c2ed7fd5d677ef1ac129f5dbad5
vagrant <vagrant@vagrant.vm>
2020-03-04 17:28:18 +0000

    finishing function

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,66 @@
+#include "lists.h"
+
+
+/**
+ * add_nodeint_head - adds a new node at the top of the list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint_head(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *current;
+
+	current = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (*head == NULL)
+    {
+	    new->next = NULL;
+		*head = new;
+    }
+	else
+	{
+        new->next = current;
+		*head = new;
+	}
+
+	return (new);
+}
+
+int is_palindrome(listint_t **head){
+    listint_t *head_inv, *current, *current_inv;
+
+	head_inv = NULL;
+    current = *head;
+
+    while (current)
+    {
+        add_nodeint_head(&head_inv, current->n);
+        current = current->next;
+    }
+
+    current = *head;
+    current_inv = head_inv;
+
+    while (current && current_inv)
+    {
+        if (current->n != current_inv->n)
+        {
+	        free_listint(head_inv);
+            return (0);
+        }
+        current = current->next;
+        current_inv = current_inv->next;        
+        
+    }
+
+	free_listint(head_inv);
+    return (1);       
+}
