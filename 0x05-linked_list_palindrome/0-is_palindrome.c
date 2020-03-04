f9c196f919b17d29900b73506284dd48b79cd890
vagrant <vagrant@vagrant.vm>
2020-03-04 17:35:33 +0000

    fixing style

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-
 /**
  * add_nodeint_head - adds a new node at the top of the list
  * @head: pointer to pointer of first node of listint_t list
@@ -21,46 +20,56 @@ listint_t *add_nodeint_head(listint_t **head, const int n)
 	new->n = n;
 
 	if (*head == NULL)
-    {
-	    new->next = NULL;
+	{
+		new->next = NULL;
 		*head = new;
-    }
+	}
 	else
 	{
-        new->next = current;
+		new->next = current;
 		*head = new;
 	}
 
 	return (new);
 }
 
-int is_palindrome(listint_t **head){
-    listint_t *head_inv, *current, *current_inv;
+/**
+ * is_palindrome - compare if the list is a palindrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 1 if is palindrome 0 if it is not
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *head_inv, *current, *current_inv;
+
+	if (!*head)
+		return (1);
 
 	head_inv = NULL;
-    current = *head;
+	current = *head;
 
-    while (current)
-    {
-        add_nodeint_head(&head_inv, current->n);
-        current = current->next;
-    }
+	while (current)
+	{
+		add_nodeint_head(&head_inv, current->n);
+		current = current->next;
+	}
 
-    current = *head;
-    current_inv = head_inv;
+	current = *head;
+	current_inv = head_inv;
 
-    while (current && current_inv)
-    {
-        if (current->n != current_inv->n)
-        {
-	        free_listint(head_inv);
-            return (0);
-        }
-        current = current->next;
-        current_inv = current_inv->next;        
-        
-    }
+	while (current && current_inv)
+	{
+		if (current->n != current_inv->n)
+		{
+			free_listint(head_inv);
+			return (0);
+		}
+		current = current->next;
+		current_inv = current_inv->next;
+
+	}
 
 	free_listint(head_inv);
-    return (1);       
+	return (1);
 }
