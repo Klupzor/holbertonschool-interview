da64cad8233fcc57850989a8156098aa79b70bc9
vagrant <vagrant@vagrant.vm>
2020-03-19 01:15:51 +0000

    adding try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -16,20 +16,28 @@ status_code = {
     500: 0
 }
 
-for arg in sys.stdin:
-    args = arg.split(" ")
-    if len(args) > 2:
-        size = args[-1]
-        code = int(args[-2])
-        if code in status_code:
-            status_code[code] += 1
+try:
+    for arg in sys.stdin:
+        args = arg.split(" ")
+        if len(args) > 2:
+            size = args[-1]
+            code = int(args[-2])
+            if code in status_code:
+                status_code[code] += 1
 
-        Tsize += int(size)
-        cont += 1
-    if cont == 10:
-        print("File size: {}".format(Tsize))
+            Tsize += int(size)
+            cont += 1
+        if cont == 10:
+            print("File size: {}".format(Tsize))
 
-        for k, v in sorted(status_code.items()):
-            if status_code[k] != 0:
-                print("{}: {}".format(k, v))
-        cont = 0
+            for k, v in sorted(status_code.items()):
+                if status_code[k] != 0:
+                    print("{}: {}".format(k, v))
+            cont = 0
+except Exception:
+    pass
+finally:
+    print("File size: {}".format(Tsize))
+    for k, v in sorted(status_code.items()):
+        if status_code[k] != 0:
+            print("{}: {}".format(k, v))
