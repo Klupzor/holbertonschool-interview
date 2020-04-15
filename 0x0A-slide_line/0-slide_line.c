6a57d43ec8b0b0661a66e2f34d68f9a1c20de63d
vagrant <vagrant@vagrant.vm>
2020-04-15 03:13:15 +0000

    finishing task

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,33 +1,78 @@
 #include "slide_line.h"
 
-int slide_line(int *line, size_t size, int direction) {
-    size_t left, right;
+/**
+ * slide_line - slides and merges an array of integers
+ *
+ * @line: Pointer to the array of integer to be printed
+ * @size: Number of elements in @array
+ * @direction: left(1) or right(2)
+ */
 
-    // left
-    if (direction == 1)
-    {
-        left = 0;
-        for ( right = 1; right < size; right++)
-        {
-            if (line[left] == line[right])
-            {
-                line[left] = line[left] * 2;
-                line[right] = 0;
-                right++;
-                while (line[right] != 0 && right < size)
-                {
-                    right++;
-                }
-                left++;
-                line[left] = line[right];
-                line[right] = 0;
-                right++;
-                
-            }
-            
-        }
-        
-    }
+int slide_line(int *line, size_t size, int direction)
+{
+	size_t left, right;
 
-    return(1);
+	if (direction == 1)
+	{
+		left = 0;
+		for (right = 1; right < size; right++)
+		{
+			if (line[left] == line[right])
+			{
+				line[left] = line[left] * 2;
+				line[right] = 0;
+				right++;
+				while (line[right] == 0)
+				{
+					if (right >= size)
+					{
+						return (1);
+					}
+
+					right++;
+				}
+				left++;
+				line[left] = line[right];
+				line[right] = 0;
+			}
+			else if (line[right] != 0)
+			{
+				left++;
+			}
+		}
+		return (1);
+	}
+
+	if (direction == 2)
+	{
+		right = size - 1;
+		for (left = size - 1; left >= 1; left--)
+		{
+			if (line[left - 1] == line[right])
+			{
+				line[right] = line[right] * 2;
+				line[left - 1] = 0;
+				left--;
+				while (line[left - 1] == 0)
+				{
+					if (left <= 1)
+					{
+						return (1);
+					}
+
+					left--;
+				}
+				right--;
+				line[right] = line[left - 1];
+				line[left - 1] = 0;
+			}
+			else if (line[left - 1] != 0)
+			{
+				right--;
+			}
+		}
+		return (1);
+	}
+
+	return (0);
 }
