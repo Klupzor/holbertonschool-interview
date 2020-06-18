c369a94f0b75a620df15f8478094f6833e25a038
vagrant <vagrant@vagrant.vm>
2020-06-18 03:45:28 +0000

    fixing header

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -6,7 +6,5 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
-void heapify(int *arr, int n, int i, size_t size);
-void swap(int *a, int *b);
 
 #endif /* HEAPSORT */
